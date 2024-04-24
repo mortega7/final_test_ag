2394c3da2ef67d69098c97197b7dd6ecc122c6d9
Manuel Alejandro Ortega <morteguita@gmail.com>
2024-04-23 21:58:19 -0500

    Task 1: Adjustment of range slider event, removing of Tailwind, use of regular css, adding buttons B2B and B2C for different values

--- task1/script.js
+++ task1/script.js
@@ -1,14 +1,24 @@
-const defaultValue = 300000;
+const defaultValueB2B = 300000;
+const defaultValueB2C = 220000;
+const defaultRanges = ['Deal', 'Prospects', 'Ratio'];
 
 document.addEventListener('DOMContentLoaded', function () {
-	document.getElementById('amount').innerHTML = formatValue(defaultValue);
+	document.getElementById('amount-b2b').innerHTML = formatValue(defaultValueB2B);
+	document.getElementById('amount-b2c').innerHTML = formatValue(defaultValueB2C);
 	document.getElementById('rangeDealLabel').innerHTML = '0';
 	document.getElementById('rangeProspectsLabel').innerHTML = '0';
 	document.getElementById('rangeRatioLabel').innerHTML = '0';
+
+	for (let rangeId of defaultRanges) {
+		const input = document.getElementById('range' + rangeId + 'Value');
+		input.addEventListener('input', () => {
+			calculateValue();
+		});
+	}
 });
 
 window.addEventListener('resize', function () {
-	showRangeLabels(['Deal', 'Prospects', 'Ratio']);
+	showRangeLabels(defaultRanges);
 });
 
 function formatValue(value) {
@@ -23,22 +33,29 @@ function calculateValue() {
 	const dealValue = document.getElementById('rangeDealValue').value;
 	const prospectsValue = document.getElementById('rangeProspectsValue').value;
 	const ratioValue = document.getElementById('rangeRatioValue').value;
-	
-	let amount = defaultValue;
-	amount -= 1000 * dealValue; //Discount 1000 per deal
-	amount += (250 * prospectsValue); //Increase 250 per prospect
-	amount -= 5 * ratioValue; //Discount 5 per ratio
 
-	document.getElementById('amount').innerHTML = formatValue(amount);
-	showRangeLabels(['Deal', 'Prospects', 'Ratio']);
+	let amountB2B = defaultValueB2B;
+	amountB2B -= 1000 * dealValue; //Discount 1000 per deal
+	amountB2B += 250 * prospectsValue; //Increase 250 per prospect
+	amountB2B -= 5 * ratioValue; //Discount 5 per ratio
+
+	let amountB2C = defaultValueB2C;
+	amountB2C -= 800 * dealValue; //Discount 800 per deal
+	amountB2C += 200 * prospectsValue; //Increase 200 per prospect
+	amountB2C -= 4 * ratioValue; //Discount 4 per ratio
+
+	document.getElementById('amount-b2b').innerHTML = formatValue(amountB2B);
+	document.getElementById('amount-b2c').innerHTML = formatValue(amountB2C);
+
+	showRangeLabels(defaultRanges);
 }
 
 function showRangeLabels(ids) {
-	if(typeof ids == 'string') {
+	if (typeof ids == 'string') {
 		ids = [ids];
 	}
 
-	for(let id of ids) {
+	for (let id of ids) {
 		const value = document.getElementById('range' + id + 'Value').value;
 		const width = document.getElementById('range' + id + 'Container').offsetWidth;
 		let left = (width * value) / 100;
@@ -57,7 +74,20 @@ function showRangeLabels(ids) {
 			left -= 20;
 		}
 
-		document.getElementById('range' + id + 'Label').innerHTML = value;
-		document.getElementById('range' + id + 'Label').style.paddingLeft = `${left}px`;
+		const element = document.getElementById('range' + id + 'Label');
+		element.innerHTML = value;
+		element.style.paddingLeft = `${left}px`;
+	}
+}
+
+function changeReach(prefixIn, prefixOut) {
+	document.getElementById(prefixIn + '-button').classList.add('active');
+	document.getElementById(prefixOut + '-button').classList.remove('active');
+
+	const hideElements = ['amount', 'text'];
+
+	for (let id of hideElements) {
+		document.getElementById(id + '-' + prefixIn).classList.remove('hidden');
+		document.getElementById(id + '-' + prefixOut).classList.add('hidden');
 	}
 }
